d0c72accf74f123f986964c41db195095069ebbc
Jeremy <Jeremycastillo901@gmail.com>
2022-02-01 19:40:55 -0400

    Creando el Contexto

---
+++ DAL/Contexto.cs
@@ -0,0 +1,9 @@
+using Microsoft.EntityFrameworkCore;
+
+public class Contexto:DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite(@"");
+    }
+}
