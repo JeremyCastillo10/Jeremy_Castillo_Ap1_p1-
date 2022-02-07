1f4732be35bf929de5c70641c0f7c54be704dfa7
Jeremy <Jeremycastillo901@gmail.com>
2022-02-07 18:30:27 -0400

    Creando el contexto y migracion

--- DAL/Contexto.cs
+++ DAL/Contexto.cs
@@ -1,9 +1,11 @@
 using Microsoft.EntityFrameworkCore;
+using Jeremy_Castillo_Ap1_p1_.Entidades;
 
 public class Contexto:DbContext
 {
+    public DbSet<Productos> Productos{ get; set; }
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        optionsBuilder.UseSqlite(@"");
+        optionsBuilder.UseSqlite(@"Data Source=DATA\Productos.db");
     }
 }
---
+++ Migrations/20220207222911_inicia.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Jeremy_Castillo_Ap1_p1_.Migrations
+{
+    public partial class inicia : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Productos",
+                columns: table => new
+                {
+                    ProductoId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Descripcion = table.Column<string>(type: "TEXT", nullable: true),
+                    Existencia = table.Column<string>(type: "TEXT", nullable: true),
+                    Costo = table.Column<float>(type: "REAL", nullable: false),
+                    ValorInventario = table.Column<float>(type: "REAL", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Productos", x => x.ProductoId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Productos");
+        }
+    }
+}
