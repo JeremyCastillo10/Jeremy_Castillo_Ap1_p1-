b27c8c653be699d0f7fe7891cb909e9720eeba04
Jeremy <Jeremycastillo901@gmail.com>
2022-02-07 18:18:00 -0400

    Creando la entidad Productos

---
+++ Entidades/Productos.cs
@@ -0,0 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Jeremy_Castillo_Ap1_p1_.Entidades
+{
+    public class Productos
+    {
+        [Key]
+        public int ProductoId { get; set; }
+        public string ?Descripcion  { get; set; }
+        public string ?Existencia { get; set; }
+        public float Costo { get; set; }
+        public float ValorInventario { get; set; }
+        
+    }
+}
