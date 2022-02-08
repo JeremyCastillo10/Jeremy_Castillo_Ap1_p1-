c2ce8601f89971a1959a28cc878632708d7fb5bf
Jeremy <Jeremycastillo901@gmail.com>
2022-02-07 20:50:51 -0400

    termine de codificar la consulta

--- UI/Consultas/consult.xaml.cs
+++ UI/Consultas/consult.xaml.cs
@@ -1,6 +1,6 @@
    
 using System.Windows;
-//using Jeremy_Castillo_Ap1_p1_.Entidades;
+using Jeremy_Castillo_Ap1_p1_.Entidades;
 using Jeremy_Castillo_Ap1_p1_.BLL;
 using System.Linq;
 using System.Collections.Generic;
@@ -17,7 +17,27 @@ namespace Jeremy_Castillo_Ap1_p1_.UI.Consultas
 
         private void BuscarButton_Click(object sender, RoutedEventArgs e)
         {
+            var lista = new List<Productos>();
 
+            if(string.IsNullOrWhiteSpace(CriterioTextBox.Text))
+            lista = ProductosBLL.GetList(l => true);
+            else{
+                switch(FiltroComboBox.SelectedIndex)
+                {
+                    case 1:
+                        lista = ProductosBLL.GetList(l => l.ProductoId.ToString().Contains(CriterioTextBox.Text));
+                        break;
+                    case 2:
+                        lista = ProductosBLL.GetList(l => l.Descripcion.Contains(CriterioTextBox.Text));
+                        break;
+                    case 3:
+                        lista = ProductosBLL.GetList(l => l.Costo.ToString().Contains(CriterioTextBox.Text));
+                        break;
+                }
+            }
+
+            DataGrid.ItemsSource = null;
+            DataGrid.ItemsSource = lista;
 
         }
 
